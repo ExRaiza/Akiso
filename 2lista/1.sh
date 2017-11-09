eaf78b4330b5e77cd7ff9b70b6070253fef4ae35
Erik Dubois <erik.dubois@gmail.com>
2017-11-10 00:30:57 +0100

    wow

--- 2lista/1.sh
+++ 2lista/1.sh
@@ -8,6 +8,9 @@ time2=0
 sum1=0
 sum2=0
 i=0
+tput civis
+#tput cvvis
+
 
 while [ 1 -eq 1 ]
 do
@@ -26,36 +29,101 @@ do
 	
 	if ! [ $sectors_read -eq $sectors_read2 ]
 	then 
-		time1=$(($SECONDS-$time1))
+		time1=$(($SECONDS-$sum1))
 		((sum1+=time1))
-		sectors_rdiff=$(($sectors_read - $sectors_read2))
+		sectors_rdiff=`echo "$sectors_read - $sectors_read2" | bc`
 		sectors_read2=$sectors_read
-		read_speed=$(( $sectors_rdiff*512/$time1 ))
+		read_speed=`echo "$sectors_rdiff*512/$time1" | bc`
 	fi
+
+	tput cup 20 0
+	printf "LOL"
+	echo `echo "$sectors_rdiff*512/$time1" >>  bc`
+
 
 	if ! [ $sectors_write -eq $sectors_write2 ] 
 	then 
 		time2=$(($SECONDS-$sum2))
 		((sum2+=time2))
-		sectors_wdiff=$(($sectors_write - $sectors_write2))
+		sectors_wdiff=`echo "$sectors_write - $sectors_write2" | bc`
 		sectors_write2=$sectors_write
 		if [ $time2 -gt 0 ]
 		then
-			write_speed=$(( $sectors_wdiff*512/$time2 ))
+			write_speed=`echo "$sectors_wdiff*512/$time2" | bc`
 
 		fi
 	fi
 
+	#tput clear
 
-	text=0;
+	arr_write[$i]=`echo "$write_speed/1000" | bc`
+	arr_read[$i]="$read_speed"
+	arr_cpu[$i]="$cpu"
+
+	IFS=$'\n'
+	maxR=`echo "${arr_read[*]}" | sort -nr | head -n1`
+	compR=$((maxR/15))
+	maxW=`echo "${arr_write[*]}" | sort -nr | head -n1`
+	compW=$((maxW/15))
+	maxC=`echo "${arr_cpu[*]}" | sort -nr | head -n1`
+	compC=`echo "$maxC / 15" |  bc`
+	IFS=
+
+	if [ $compR -eq 0 ] 
+	then
+		compR=15
+	fi
+
+	if [ $compW -eq 0 ] 
+	then
+		compW=15
+	fi
+
+	if [ $compC -eq 0 ] 
+	then
+		compC=15
+	fi
+
+	for counter in ${!arr_write[@]}
+    do 
+    	tput cup $(($counter+1)) 0
+    	tput setab 1
+    	length=$(( ${arr_write[counter]}/$compW ))
+		printf '%*s' "$length"
+		tput sgr0
+		printf " ${arr_write[counter]} KB/s"
+    done
+
+    for counter in ${!arr_read[@]}
+    do 
+    	tput cup $(($counter+1)) 30
+    	tput setab 1
+    	length=$(( ${arr_read[counter]}/$compR ))
+		printf '%*s' "$length"
+		tput sgr0
+		printf " ${arr_read[counter]} B/s"
+    done
+
+    for counter in ${!arr_cpu[@]}
+    do 
+    	tput cup $(($counter+1)) 60
+    	tput setab 1
+    	length=`echo "${arr_cpu[counter]}/$compC" | bc `
+		printf '%*s' "$length"
+		tput sgr0
+		printf " ${arr_cpu[counter]}"
+    done
+
+
+    text=0;
 	text2=0
 	if [ $write_speed -gt 1000 ] && [ $write_speed -lt 1000000 ] 
 	then
-		write_speed=$(($write_speed/1000))
+		write_speed=`echo "$write_speed/1000" | bc`
 		text="write $write_speed KB/s"
 	elif [ $write_speed  -gt 1000000 ] 
 	then
-		write_speed=$(($write_speed/1000000))
+		write_speed=`echo "$write_speed/1000000" | bc`
 		text="write $write_speed MB/s"
 	else
 		text="write $write_speed B/s"
@@ -73,25 +141,23 @@ do
 		text2="read $read_speed B/s"
 	fi
 
-	clear
+	tput cup 0 0
 	echo "$text"
+	tput cup 0 30
 	echo "$text2"
-	echo "CPU $cpu"
+	tput cup 0 60
+	#printf "LOL"
+	#echo "CPU $cpu"
 
-	arr[$i]=$cpu
-	((i++))
-	echo "${arr[*]}"
-	if [ $i -gt 9 ] 
+	if [ $i -gt 8 ] 
 	then
 		((i--))
-		for counter in {0..9}
+		for counter in {0..8}
         do
-            arr[$counter]=${arr[$(($counter+1))]}
-            echo ${arr[$counter]}
+            arr_write[$counter]=${arr_write[$(($counter+1))]}
+            arr_read[$counter]=${arr_read[$(($counter+1))]}
         done
 	fi
+
+	((i++))
 done
-
-
-
-
