738f51de628ac97e8eee97d9855a64cc1a9df1df
Erik Dubois <erik.dubois@gmail.com>
2017-11-12 01:12:48 +0100

    working with buffer

--- 2lista/2.sh
+++ 2lista/2.sh
@@ -2,22 +2,35 @@
 
 printf "PID    | PPID    | STATE | PGRP    | SESS    | TTY | OPEN FILES \n"
 
-find /proc -maxdepth 1 -type d | grep -o '[0-9]*' | while read dire
+find /proc -mindepth 2 -maxdepth 2 -type f -name stat | while read dire
 do
-	cd /proc/$dire 2>/dev/null
-	t1=`cat stat | awk '{print $1}'`
-	t2=`cat stat | awk '{print $4}'`
-	t3=`cat stat | awk '{print $3}'`
-	t4=`cat stat | awk '{print $5}'`
-	t5=`cat stat | awk '{print $6}'`
-	t6=`cat stat | awk '{print $7}'`
+	#cd /proc/$dire 2>/dev/null
+	t1=`cat $dire 2> /dev/null | awk '{print $1}'`
+	t2=`cat $dire 2> /dev/null | awk '{print $4}'`
+	t3=`cat $dire 2> /dev/null | awk '{print $3}'`
+	t4=`cat $dire 2> /dev/null | awk '{print $5}'`
+	t5=`cat $dire 2> /dev/null | awk '{print $6}'`
+	t6=`cat $dire 2> /dev/null | awk '{print $7}'`
 	#printf "$t1 ${#t1}"
-	printf "$t1%*s" $((8-${#t1})) 
-	printf "$t2%*s" $((10-${#t2}))
-	printf "$t3%*s" $((8-${#t3}))
-	printf "$t4%*s" $((10-${#t4}))
-	printf "$t5%*s" $((10-${#t5}))
-	printf "$t6%*s" $((6-${#t6}))
-	open=`ls ./fd | wc -l`
-	printf " $open\n"
+	if [[ $t1 != "" ]]
+	then
+		bufor=$t1
+		txt=`printf "%*s" "$((8-${#t1}))"`
+		bufor="$bufor$txt"
+		txt=`printf "$t2%*s" $((10-${#t2}))`
+		bufor="$bufor$txt"
+		txt=`printf "$t3%*s" $((8-${#t3}))`
+		bufor="$bufor$txt"
+		txt=`printf "$t4%*s" $((10-${#t4}))`
+		bufor="$bufor$txt"
+		txt=`printf "$t5%*s" $((10-${#t5}))`
+		bufor="$bufor$txt"
+		txt=`printf "$t6%*s" $((6-${#t6}))`
+		bufor="$bufor$txt"
+		dire=${dire%/*}
+		open=`ls $dire/fd 2> /dev/null | wc -l`
+		txt=`printf "$open \n"`
+		bufor="$bufor$txt"
+		echo -e "$bufor"
+	fi
 done
